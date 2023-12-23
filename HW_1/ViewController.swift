375822f23ef0a3e981b2d3e1fa1e4aadb9a7eda0
Chloe <ichunchen0927@gmail.com>
2023-12-23 15:58:55 +0800

    Initial Commit

---
+++ HW_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_1
+//
+//  Created by Chloe on 2023/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
